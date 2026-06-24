65fc1d9a5c1bee6a54bb73a8c447495dfc7dc6a8
jasonbenedict <jason_benedict_lee@dlsu.edu.ph>
2026-06-24 14:21:47 +0800

    Override equals in Coordinate class to make sure equals means same coordinates and not same object

--- release/sokobot/src/solver/Coordinate.java
+++ release/sokobot/src/solver/Coordinate.java
@@ -9,4 +9,12 @@ public class Coordinate {
         this.y = y;
     }
 
+    // Override equals method to make sure equals means same coordinates and not same objects.
+    @Override public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Coordinate)) return false;
+        Coordinate other = (Coordinate) obj;
+        return this.x ==  other.x && this.y == other.y;
+    }
+
 }
