c8d9218efb5be77f5be3e05cbdcffd58332eac11
harrisgarzonn <harris_martin_garzon@dlsu.edu.ph>
2026-06-25 20:57:21 +0800

    Update State.java

--- release/sokobot/src/solver/State.java
+++ release/sokobot/src/solver/State.java
@@ -5,18 +5,18 @@ import java.util.*;
 public class State implements Comparable<State> {
     public final Coordinate player;
     public final Set<Coordinate> crates;
-    public final String path;
+    public final char move;
     public final State predecessor;
     public int h, g, f;
 
-    public State(Coordinate player, Set<Coordinate> crates, String path, State pre, int h, int g) {
+    public State(Coordinate player, Set<Coordinate> crates, char move, State pre, int h, int g) {
         this.player = player;
         this.crates = crates;
-        this.path = path;
+        this.move = move;
         this.predecessor = pre;
         this.h = h;
         this.g = g;
-        this.f = g+h;
+        this.f = g + h;
     }
 
     @Override
@@ -24,15 +24,16 @@ public class State implements Comparable<State> {
         if (this == obj) return true;
         if (!(obj instanceof State)) return false;
         State other = (State) obj;
-
-        // Two states are identical if the player position matches 
-        // AND all boxes are in the exact same positions
         return this.player.equals(other.player) && this.crates.equals(other.crates);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(player, crates);
     }
 
     @Override
     public int compareTo(State other) {
         return Integer.compare(this.f, other.f);
     }
-
 }
