2fbadfdee2b50d12dea8d5509cfaa4676ec99b02
harrisgarzonn <harris_martin_garzon@dlsu.edu.ph>
2026-06-25 20:56:55 +0800

    Update Coordinate.java

--- release/sokobot/src/solver/Coordinate.java
+++ release/sokobot/src/solver/Coordinate.java
@@ -1,20 +1,26 @@
 package solver;
 
+import java.util.Objects;
+
 public class Coordinate {
-    private final int x;
-    private final int y;
+    public final int x; // Made public so SokoBot can access them
+    public final int y;
 
     public Coordinate(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    // Override equals method to make sure equals means same coordinates and not same objects.
-    @Override public boolean equals(Object obj) {
+    @Override 
+    public boolean equals(Object obj) {
         if (this == obj) return true;
         if (!(obj instanceof Coordinate)) return false;
         Coordinate other = (Coordinate) obj;
-        return this.x ==  other.x && this.y == other.y;
+        return this.x == other.x && this.y == other.y;
     }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
 }
